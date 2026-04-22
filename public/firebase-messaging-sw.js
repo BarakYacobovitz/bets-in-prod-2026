b81f0daab362cc1f81780eb4522a4d8bc7f3d4e9
barak <bawak.y10@gmail.com>
2026-04-22 11:07:21 +0300

    Fix: fix bedge

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,12 +6,14 @@ importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-comp
 
 // --- ⚠️ החלף את זה בנתונים האמיתיים שלך מקובץ ה-firebase.ts ⚠️ ---
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  
+  apiKey: "AIzaSyB1bwnonDx4ZoRZD_32Tdozh8T6iazxiKk",
+  authDomain: "betsinprod26.firebaseapp.com",
+  projectId: "betsinprod26",
+  storageBucket: "betsinprod26.firebasestorage.app",
+  messagingSenderId: "948458575430",
+  appId: "1:948458575430:web:6261da007a3ec9fb0f1c47"
+
 };
 // ---------------------------------------------------------------
 
